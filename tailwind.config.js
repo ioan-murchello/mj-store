f50c42c2067f932265ce40621970e1f95ff05a27
Jack <mihalkurz@gmail.com>
2024-03-10 16:12:04 +0200

    second try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
-import daisyui from 'daisyui';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
-    '/public/index.html',
+    './src/components/**/*.{html,js}',
+    './public/index.html',
   ],
   theme: {
     extend: {},
